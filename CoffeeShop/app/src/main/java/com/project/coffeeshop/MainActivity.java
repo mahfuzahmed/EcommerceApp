014db8d2377f07ba92369625811ba27ecc70e6bf
Mahfuz Ahmed <mahfuz96@hotmail.com>
2019-10-05 23:59:28 +0600

    All files copied from Android Studio deafult save directory

---
+++ CoffeeShop/app/src/main/java/com/project/coffeeshop/MainActivity.java
@@ -0,0 +1,51 @@
+package com.project.coffeeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageView background;
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //imageview
+        background = findViewById(R.id.background);
+        logo = findViewById(R.id.logo);
+
+        background.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this,ordercoffee.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        logo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this,ordercoffee.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+    }
+}
---
+++ CoffeeShop/app/src/main/java/com/project/coffeeshop/ordercoffee.java
@@ -0,0 +1,15 @@
+package com.project.coffeeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ordercoffee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordercoffee);
+    }
+}
